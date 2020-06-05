755e99ca166d4b052f850d8eba4cb1c01d7ab602
TheCats100 <m-g-152@outlook.fr>
2020-06-05 16:47:42 +0200

    feat(route): get, post, put (less 400)

--- route.js
+++ route.js
@@ -23,7 +23,9 @@ app.get('/', (request, response) => {
 app.get('/users', (req, res) => {
   connection.query('SELECT * from DVM_Legal_Entity', (err, results) =>{
     if(err) {
-      res.status(500).send('No body was here..')
+      return (
+        res.status(500).send('No body was here..')
+      )
     }
     res.json(results);
   })
@@ -33,48 +35,65 @@ app.get('/users/:id', (req, res) => {
   const { id } = req.params
   connection.query('SELECT * from DVM_Legal_Entity WHERE id = ?', id, (err, results) => {
     if(err) {
-      res.status(500).send('Internal server error')
+      return (
+        res.status(500).send('Internal server error')
+      )
     }
     if (results.length === 0) {
-      res.status(404).send('User ID not found')
-    } else {
-      res.json(results)
+      return (
+        res.status(404).send('User ID not found')
+      )
     }
+    res.json(results)
   })
 })
 
 app.post('/users', (req, res) => {
   const formData = req.body;
   if (formData.ordinal_number === null || formData.lastname === null){
-    res.status(400).send("Necessary fields empty")
+    return (
+      res.status(400).send("Necessary fields empty")
+    )
   }
   connection.query('INSERT INTO DVM_Legal_Entity SET ?', formData, (err, results) => {
     if (err) {
-      res.status(500).send("Erreur lors de la sauvegarde d'un DVM");
+      return (
+        res.status(500).send("Erreur lors de la sauvegarde d'un DVM")
+      )
     }
-    res.status(201).send('coucou steph')
+    res.status(201).json({...formData, id : results.insertId})
   });
 });
 
 app.put('/users/:id', (req, res) => {
+
   const idDVM = req.params.id;
   const formData = req.body;
+
   connection.query('UPDATE DVM_Legal_Entity SET ? WHERE id = ?', [formData, idDVM], (err, results) => {
     if (err) {
-      console.log(err);
-      res.status(500).send("Erreur lors de la modification d'un DVM");
-    } else {
-      res.sendStatus(200);
+      return (
+        res.status(500).send("Error server")
+      )
     }
+    if (results.changedRows === 0) {
+      return (
+        res.status(404).send('User ID not found')
+      )
+    }
+    res.status(200).send(`Changed row ${results.changedRows}`);
   });
+
 });
 
 app.delete('/users/:id', (req, res) => {
   const idDVM = req.params.id;
+
   connection.query('DELETE FROM DVM_Legal_Entity WHERE id = ?', [idDVM], err => {
     if (err) {
-      console.log(err);
-      res.status(500).send("Erreur lors de la suppression d'un DVM");
+      return (
+        res.status(500).send("Erreur lors de la suppression d'un DVM")
+      )
     } else {
       res.sendStatus(200);
     }
