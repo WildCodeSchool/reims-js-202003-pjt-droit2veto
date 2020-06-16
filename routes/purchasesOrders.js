4aed1ec71287ab9428b27e3f4099ae92e80b6dde
angelique-m08 <mora.angelique@gmail.com>
2020-06-16 11:31:36 +0200

    feat(purchasesorders): create purchasesorders files with router

---
+++ routes/purchasesOrders.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const connection = require('../conf');
+const router = express.Router({ mergeParams: true });
+
+router.get('/', (req, res) => {
+  connection.query('SELECT * from PurchasesOrders', (err, results) => {
+    if (err) {
+      return (
+        res.status(500).send('There are no purchasesorders...')
+      )
+    }
+    res.json(results);
+  })
+});
+
+router.get('/:id', (req, res) => {
+  connection.query('SELECT * from PurchasesOrders WHERE id = ?', id, (err, results) => {
+    if (err) {
+      return (
+        res.status(500).send('Internal server error')
+      )
+    }
+    if (results.length === 0) {
+      return (
+        res.status(404).send('PurchasesOrders ID not found')
+      )
+    }
+    res.json(results)
+  })
+});
+
+router.post('/', (req, res) => {
+  const formData = req.body;
+  if (formData.title === null) {
+    return (
+      res.status(400).send("Necessary fields are empty")
+    )
+  }
+  connection.query('INSERT INTO PurchasesOrders SET ?', formData, (err, results) => {
+    if (err) {
+      return (
+        res.status(500).send("Internal server error")
+      )
+    } else{
+      return (
+        res.status(201).json({ ...formData, id: results.insertId })
+      )
+    }
+  });
+});
+
+module.exports = router;
