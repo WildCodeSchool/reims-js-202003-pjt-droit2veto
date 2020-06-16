d9fd7a9cc4a38c56c8e81a475d0e6c4191ddb40b
ceecyl.lang@gmail.com <ceecyl.lang@gmail.com>
2020-06-16 15:22:39 +0200

    edit(products change send on json)

--- routes/products.js
+++ routes/products.js
@@ -14,7 +14,7 @@ router.delete('/:id', (req, res) => {
   connection.query('DELETE FROM Products WHERE id = ?', [idProducts], err => {
     if (err) {
       return (
-        res.status(500).send("Internal server error")
+        res.status(500).json({message: "Internal server error"})
       )
     } else {
       res.sendStatus(200);
@@ -29,21 +29,21 @@ router.put('/:id', (req, res) => {
 
   if (isNaN(idProducts)) {
     return (
-      res.status(400).send("No correct ID")
+      res.status(400).json({message:"No correct ID"})
     )
   }
   connection.query('UPDATE products SET ? WHERE id = ?', [formData, idProducts], (err, results) => {
     if (err) {
       return (
-        res.status(500).send("Error server")
+        res.status(500).json({message: "Error server"})
       )
     }
     if (results.changedRows === 0) {
       return (
-        res.status(404).send('User ID not found')
+        res.status(404).json({message: 'User ID not found'})
       )
     }
-    res.status(200).send(`Changed row ${results.changedRows}`);
+    res.status(200).json(`Changed row ${results.changedRows}`);
   });
 
 });
@@ -52,13 +52,13 @@ router.post('/', (req, res) => {
   const formData = req.body;
   if (formData.title === null) {
     return (
-      res.status(400).send("Necessary fields empty")
+      res.status(400).json({message: "Necessary fields empty"})
     )
   }
   connection.query('INSERT INTO Products SET ?', formData, (err, results) => {
     if (err) {
       return (
-        res.status(500).send("Internal server error")
+        res.status(500).json({message: "Internal server error"})
       )
     } else {
       return (
@@ -73,12 +73,12 @@ router.get('/:id', (req, res) => {
   connection.query('SELECT * from products WHERE id = ?', id, (err, results) => {
     if (err) {
       return (
-        res.status(500).send('Internal server error')
+        res.status(500).json({message: 'Internal server error'})
       )
     }
     if (results.length === 0) {
       return (
-        res.status(404).send('products ID not found')
+        res.status(404).json({message: 'products ID not found'})
       )
     }
     res.json(results)
