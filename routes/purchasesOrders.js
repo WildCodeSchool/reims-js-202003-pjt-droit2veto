faeab48d561988ab755f861690fe1934129a8a37
WildSteph <Stephaine.Pantiez@gmail.com>
2020-06-16 16:48:54 +0200

    refactor(puscharsesorders): add bad id

--- routes/purchasesOrders.js
+++ routes/purchasesOrders.js
@@ -14,7 +14,14 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+  const { id } = req.params
+  if (isNaN(id)) {
+    return (
+      res.status(400).json({message: "No correct ID"})
+    )
+  }
   connection.query('SELECT * from PurchasesOrders WHERE id = ?', id, (err, results) => {
+    
     if (err) {
       return (
         res.status(500).json({message: 'Internal server error'})
