e82609de456e11fab504dbdfc210e4a853f15147
WildSteph <Stephaine.Pantiez@gmail.com>
2020-06-17 09:32:19 +0200

    refactor(tete): add 400 and 201 for post to purchasesOrders

--- test/app.integration.spec.js
+++ test/app.integration.spec.js
@@ -90,14 +90,14 @@ describe('Test DVM_Legal_Entity :', () => {
   it('POST / Correct', (done) => {
     request(app)
       .post('/users')
-      .send({ 
+      .send({
         lastname: "Rousseau",
-        ordinal_number: "654321" 
+        ordinal_number: "654321"
       })
       .expect(201)
       .expect('Content-Type', /json/)
       .then(response => {
-        const expected = {id: expect.any(Number), lastname: "Rousseau", ordinal_number: "654321"}
+        const expected = { id: expect.any(Number), lastname: "Rousseau", ordinal_number: "654321" }
         expect(response.body).toEqual(expected);
         done();
       });
@@ -136,7 +136,7 @@ describe('Test DVM_Legal_Entity :', () => {
       .expect(200)
       .expect('Content-Type', /json/)
       .then(response => {
-        const expected = {message: 'Changed row 1' }
+        const expected = { message: 'Changed row 1' }
         expect(response.body).toEqual(expected)
         done();
       });
@@ -202,6 +202,39 @@ it('GET / Id purchasesOrders : Correct', (done) => {
     .then(response => {
       const expected = { DVM_id: expect.any(Number), id: expect.any(Number), quantity: expect.any(Number), }
       expect(response.body[0]).toEqual(expected)
+      done();
+    });
+});
+
+
+it('POST / field missing from purchasesOrders', (done) => {
+  request(app)
+    .post('/purchasesOrders')
+    .send({
+      DVM_id: "",
+      quantity: ""
+    })
+    .expect(400)
+    .expect('Content-Type', /json/)
+    .then(response => {
+      const expected = { message: 'Necessary fields are empty' }
+      expect(response.body).toEqual(expected);
+      done();
+    });
+});
+
+it('POST / Correct from purchasesOrders', (done) => {
+  request(app)
+    .post('/purchasesOrders')
+    .send({
+      DVM_id: "1",
+      quantity: "10"
+    })
+    .expect(201)
+    .expect('Content-Type', /json/)
+    .then(response => {
+      const expected = { id: expect.any(Number), DVM_id: "1", quantity: "10" }
+      expect(response.body).toEqual(expected);
       done();
     });
 });
