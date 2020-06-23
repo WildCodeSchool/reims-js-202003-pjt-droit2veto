b1be6b18efbfd33e48063c292e24a16c4d7d7098
TheCats100 <m-g-152@outlook.fr>
2020-06-23 09:28:13 +0200

    feat(app): add app.use(cors)

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const api = require('./routes');
+const cors = require('cors')
 
 const app = express();
 const port = process.env.port || 8000;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
