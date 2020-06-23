97e76cc53528c6d1f2c57133a141539b7cdb6bec
WildSteph <Stephaine.Pantiez@gmail.com>
2020-06-23 09:55:20 +0200

    refactor(products.js): delete isnan

--- routes/products.js
+++ routes/products.js
@@ -67,11 +67,6 @@ router.post('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params
-  if (isNaN(id)) {
-    return (
-      res.status(400).json({message: "No correct ID"})
-    )
-  }
   connection.query('SELECT * from Products WHERE id = ?', id, (err, results) => {
     if (err) {
       return (
