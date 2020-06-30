59a28b0b2133d3bf726305d8bf23602326f39253
WildSteph <Stephaine.Pantiez@gmail.com>
2020-06-30 15:11:49 +0200

    refactor(users): add hash

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const connection = require('../conf');
 const router = express.Router({ mergeParams: true });
+const bcrypt = require('bcrypt');
+
 
 router.get('/', (req, res) => {
   connection.query('SELECT * from DVM_Legal_Entity', (err, results) => {
     if (err) {
       return (
-        res.status(500).json({message: 'No body was here..'})
+        res.status(500).json({ message: 'No body was here..' })
       )
     }
     res.json(results);
@@ -18,12 +20,12 @@ router.get('/:id', (req, res) => {
   connection.query('SELECT * from DVM_Legal_Entity WHERE id = ?', id, (err, results) => {
     if (err) {
       return (
-        res.status(500).json({message: 'Internal server error'})
+        res.status(500).json({ message: 'Internal server error' })
       )
     }
     if (results.length === 0) {
       return (
-        res.status(404).json({message: 'User ID not found'})
+        res.status(404).json({ message: 'User ID not found' })
       )
     }
     res.json(results)
@@ -32,18 +34,26 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   const formData = req.body;
-  if (formData.ordinal_number == null || formData.email == null || formData.password == null ) {
+  if (formData.ordinal_number == null || formData.email == null || formData.password == null) {
     return (
-      res.status(400).json({message: "Necessary fields are empty"})
-    )
+      res.status(400).json({ message: "Necessary fields are empty" })
+    );
   }
-  connection.query('INSERT INTO DVM_Legal_Entity SET ?', formData, (err, results) => {
-    if (err) {
-      return (
-        res.status(500).json({message: "Erreur lors de la sauvegarde d'un DVM"})
-      )
+  bcrypt.hash(formData.password, 10, function (err, hash) {
+    formData.password = hash;
+    console.log(formData.password);
+    if (res) {
+      connection.query('INSERT INTO DVM_Legal_Entity SET ?', formData, (err, results) => {
+        if (err) {
+          return (
+            res.status(500).json({ message: "Erreur lors de la sauvegarde d'un DVM" })
+          );
+        }
+        res.status(201).json({ ...formData, id: results.insertId })
+      });
+    } else {
+      res.status(500).json({ message: "pass no hash" });
     }
-    res.status(201).json({ ...formData, id: results.insertId })
   });
 });
 
@@ -55,15 +65,15 @@ router.put('/:id', (req, res) => {
   connection.query('UPDATE DVM_Legal_Entity SET ? WHERE id = ?', [formData, idDVM], (err, results) => {
     if (err) {
       return (
-        res.status(500).json({message: "Error server"})
+        res.status(500).json({ message: "Error server" })
       )
     }
     if (results.changedRows === 0) {
       return (
-        res.status(404).json({message: 'User ID not found'})
+        res.status(404).json({ message: 'User ID not found' })
       )
     }
-    res.status(200).json({message: `Changed row ${results.changedRows}`});
+    res.status(200).json({ message: `Changed row ${results.changedRows}` });
   });
 });
 
@@ -73,7 +83,7 @@ router.delete('/:id', (req, res) => {
   connection.query('DELETE FROM DVM_Legal_Entity WHERE id = ?', [idDVM], err => {
     if (err) {
       return (
-        res.status(500).json({message: "Internal server error"})
+        res.status(500).json({ message: "Internal server error" })
       )
     } else {
       res.sendStatus(200);
