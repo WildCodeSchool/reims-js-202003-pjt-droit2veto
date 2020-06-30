7f7356d2382a61a49171366b566df01ffa6d6893
angelique-m08 <mora.angelique@gmail.com>
2020-06-30 16:45:49 +0200

    feat(auth): create auth file for router post login

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { createToken } = require('../services/jwt');
+const User = require('../models/User');
 
-router.post('/login', loginUser);
+router.post('/login', (req, res) => {
+  User.findByEmailAndPassword(req.body.email, req.body.password, (err, user) => {
+    if (err) {
+      return (
+        res.status(500).json({ error: err })
+      );
+    } 
+    if (!user) {
+      return (
+        res.json({ error: 'email ou mot de passe incorrect' })
+      );
+    }
+    const token = createToken(user);
+    res.json({ user, token });
+  });
+});
 
 module.exports = router;
