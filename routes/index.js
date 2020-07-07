48927fe9537448120af8c0703793521a2aa04842
angelique-m08 <mora.angelique@gmail.com>
2020-07-07 17:17:03 +0200

    refactor(index): add router for usersactivities

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const purchasesOrders = require('./purchasesOrders');
 const products = require('./products');
 const activitiesProducts = require('./activities_products')
 const login = require('./auth');
+const usersActivities = require('./users_activities');
 
 const router = express.Router();
 
@@ -15,6 +16,7 @@ router.use('/purchasesorders', purchasesOrders);
 router.use('/products', products);
 router.use('/activitiesproducts', activitiesProducts);
 router.use('/login', login);
+router.use('/usersactivities', usersActivities);
 
 module.exports = router;
 
