78ab0a828abeca962ff1877731e25db3d8d2feb4
WildSteph <Stephaine.Pantiez@gmail.com>
2020-07-07 15:25:58 +0200

    refacor(users) : back at  ...formData, id: results.insertId

--- routes/users.js
+++ routes/users.js
@@ -49,7 +49,7 @@ router.post('/', (req, res) => {
             res.status(500).json({ ...formData, id: results.insertId})
           );
         }
-        res.status(201).json({ message: `User created with id: ${results.insertId}` });
+        res.status(201).json({ ...formData, id: results.insertId });
       });
     } else {
       res.status(500).json({ message: "pass no hash" });
