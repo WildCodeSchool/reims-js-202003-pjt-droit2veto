4be81af81299064bd9a500add7d815e46db4aa1a
WildSteph <Stephaine.Pantiez@gmail.com>
2020-07-08 09:33:04 +0200

    refactor(droitdeveto): add all for DVM table

--- droitdeveto.sql
+++ droitdeveto.sql
@@ -11,7 +11,15 @@ CREATE TABLE DVM_Legal_Entity (
   email VARCHAR(100) NOT NULL,
   password VARCHAR(100)NOT NULL,
   lastname VARCHAR(100), 
-  firstname VARCHAR(100), 
+  firstname VARCHAR(100),
+  establishment_name VARCHAR(100),
+  address_of_establishment_1 VARCHAR (100),
+  address_of_establishment_2 VARCHAR (100),
+  city VARCHAR(100),
+  postal_code VARCHAR(100),
+  telephone_of_the_establishment VARCHAR(100),
+  personal_phone VARCHAR(100),
+  logo varchar(100),
   ordinal_number VARCHAR(6) 
 );
 
