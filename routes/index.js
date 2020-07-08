07f2e5db5022da3ef83e3eb90778562fa32322a8
angelique-m08 <mora.angelique@gmail.com>
2020-07-08 09:27:16 +0200

    refactor(users): change url route and add it in users

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ const purchasesOrders = require('./purchasesOrders');
 const products = require('./products');
 const activitiesProducts = require('./activities_products')
 const login = require('./auth');
-const usersActivities = require('./users_activities');
 
 const router = express.Router();
 
@@ -16,7 +15,6 @@ router.use('/purchasesorders', purchasesOrders);
 router.use('/products', products);
 router.use('/activitiesproducts', activitiesProducts);
 router.use('/login', login);
-router.use('/usersactivities', usersActivities);
 
 module.exports = router;
 
--- routes/users.js
+++ routes/users.js
@@ -93,6 +93,18 @@ router.delete('/:id', (req, res) => {
 
 /*******************************************/
 
+router.get('/:userId/activities', (req, res) => {
+  const userId = req.params.userId;
+  connection.query('SELECT * FROM Activities a JOIN DVM_Activities da ON a.id=da.Activities_id WHERE DVM_id = ?', userId, (err, results) => {
+    if (err) {
+      return (
+        res.status(500).json({ message: 'Error server' })
+      )
+    }
+    res.json(results);
+  });
+});
+
 
 
 module.exports = router;
