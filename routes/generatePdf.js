f030e8c308ab91b4ffb8d8ffcbbc7f2fcb71204b
angelique-m08 <mora.angelique@gmail.com>
2020-07-15 17:50:08 +0200

    refactor(generatepdf): add query to get user informations

--- routes/generatePdf.js
+++ routes/generatePdf.js
@@ -5,21 +5,26 @@ const pdfTemplate = require('../documents/pdf');
 const pdf = require('html-pdf'); 
 
 router.post('/:userId/activities', (req, res) => {
-  const userId = req.params.userId;
-  connection.query('SELECT * FROM Activities a JOIN DVM_Activities da ON a.id=da.Activities_id WHERE DVM_id = ?', userId, (err, results) => {
+  const { userId } = req.params;
+  connection.query('SELECT * FROM Activities a JOIN DVM_Activities da ON a.id=da.Activities_id WHERE DVM_id = ?', userId, (err, activities) => {
     if (err) {
       return (
         res.status(500).json({ message: 'Error server' })
       )
     }
-    pdf.create(pdfTemplate(results), {}).toFile(`${__dirname}/result.pdf`, (err) => {
-      console.log(results)
+  connection.query('SELECT * FROM DVM_Legal_Entity WHERE id = ?', userId, (err, user) => {
+    if (err) {
+      return (
+        res.status(500).json({ message: 'Error server' })
+      )
+    }
+    pdf.create(pdfTemplate(activities, user), {}).toFile(`${__dirname}/result.pdf`, (err) => {
       if(err) {
         return res.sendStatus(500)
       }
       return res.sendStatus(200)
     });
-  });
+  })});
 });
 
 router.get('/:userId/pdf', (req, res) => {
