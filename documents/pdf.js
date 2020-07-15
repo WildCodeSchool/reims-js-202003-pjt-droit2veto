003a31351c0a110af260628602356a0d0fdca4f9
angelique-m08 <mora.angelique@gmail.com>
2020-07-15 15:27:16 +0200

    refactor(pdf): add reduce to get activities titles and description

--- documents/pdf.js
+++ documents/pdf.js
@@ -1,14 +1,22 @@
-module.exports = () => {
+module.exports = (results) => {
   return`
     <!DOCTYPE html>
     <html>
       <head>
         <meta charset="utf-8">
         <title>Droit2Veto pdf</title>
+        <style>
+        h1 {
+          color: red;
+        }
+        </style>
       </head>
       <body>
         <div>
           <h1>Mes activités</h1>
+          <ul>${results.reduce((acc, currentValue) => {
+            return acc+'<li>'+currentValue.title+','+currentValue.description+'</li>'
+          }, '')}</ul>
         </div>
       </body>
     </html>
